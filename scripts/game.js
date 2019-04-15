49a7270789c807e8bd8a525e6e24d0429d927448
zvncnt <zoe.vincent264@gmail.com>
2019-04-15 16:35:43 +0200

    asteroid class

--- scripts/game.js
+++ scripts/game.js
@@ -12,3 +12,18 @@ context.lineTo(50, 200)  // Tracer autre une ligne
 context.fill()           // Faire apparaitre la forme dessinée
 
 // Asteroïdes
+
+class Asteroid{
+  constructor(diameter){
+    this.diameter=diameter
+  }
+  display(){
+    context.beginPath();
+    context.fillStyle="#FF4422"
+    context.arc(80, 80, 70, 0, 2 * Math.PI);
+    context.fill();
+  }
+}
+
+let asteroid1 = new Asteroid(100)
+asteroid1.display()
