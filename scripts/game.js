967738cd0b685023c5b783658b80cb042458b576
zvncnt <zoe.vincent264@gmail.com>
2019-04-15 11:59:09 +0200

    canvas

---
+++ scripts/game.js
@@ -0,0 +1,10 @@
+const canvas = document.querySelector('canvas')
+const context = canvas.getContext('2d')
+
+context.beginPath()      // Commencer un tracé
+
+context.moveTo(50, 50)   // Placer le tracé
+context.lineTo(200, 200) // Tracer une ligne
+context.lineTo(50, 200)  // Tracer autre une ligne
+
+context.fill()           // Faire apparaitre la forme dessinée
