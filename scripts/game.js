a22786623fed6513f3091bed6ad2ef5d7a47aff4
zvncnt <zoe.vincent264@gmail.com>
2019-04-16 14:56:08 +0200

    animation canvas

--- scripts/game.js
+++ scripts/game.js
@@ -1,11 +1,17 @@
 /* VARIABLES*/
-let canvas    = document.querySelector('canvas'),
-    context   = canvas.getContext('2d'),
-    score = 0
-
+let canvas=document.querySelector('canvas'),
+    context=canvas.getContext('2d'),
+    score=0
+// Asteroid
 const tRadius=[20, 30, 40]
 let asteroids = new Array(5)
 let posX, posY, radius, indexRadius
+//Fond
+const gradient = context.createRadialGradient(
+    400, 300, 100,
+    400, 300, 400,
+    400, 300, 600,
+)
 
 /*OBJETS*/
 //Spaceship
@@ -28,7 +34,32 @@ class Asteroid{
 
 /* GAME */
 
-generateAsteroid()
+let asteroidtest=new Asteroid(20, 20, 30)
+
+const loop = () =>
+{
+    window.requestAnimationFrame(loop)
+    // Mise à jour des coordonnées
+    asteroidtest.posY += 5
+    // Efface le canvas
+    gradient.addColorStop(0, '#6b35ab')    // Couleur de départ
+    gradient.addColorStop(0.5, ' #322778') // Couleur de milieu
+    gradient.addColorStop(1, '#041444') // Couleur de arrivée
+    context.fillStyle = gradient  // Le dégradé devient le style de remplissage
+    // context.fillStyle = '#041444'
+    context.fillRect(0, 0, canvas.width, canvas.height)
+    // Dessine l'asteroide
+    asteroidtest.draw()
+}
+loop()
+
+
+// let play= setInterval(
+//     function(){
+//     generateAsteroid()
+//   },
+//   1000
+//   )
 
 
 /*FUNCTIONS*/
@@ -40,12 +71,10 @@ generateAsteroid()
 
 function generateAsteroid(){
   for (let i = 0; i < asteroids.length; i++){
-    console.log(asteroids);
     posY=20
     posX=Math.floor(Math.random()*800)
     indexRadius=Math.floor(Math.random()*3)
     radius=tRadius[indexRadius]
     asteroids[i] = new Asteroid(posX, posY, radius)
-    asteroids[i].draw()
   }
 }
