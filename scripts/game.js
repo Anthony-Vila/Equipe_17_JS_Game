645f62c4fd08e7b4b0b559a64f3f4b761999ea51
zvncnt <zoe.vincent264@gmail.com>
2019-04-16 10:22:30 +0200

    canvas size modification

--- scripts/game.js
+++ scripts/game.js
@@ -10,7 +10,29 @@ var canvas    = document.querySelector('canvas'),
     asteroid_destroyed = 0,
     score = 0;
 
-var score_munition_update;
+var score_munition_update
+
+
+// Jeu
+
+init()
+
+function init()
+{
+  //initialisation
+  score = 0
+
+  //lancement du jeu
+  generateAsteroid()
+
+  let play= setInterval(
+    function(){
+    asteroidFall()
+  },
+  1000
+  )
+}
+
 
 // Spaceship
 
@@ -20,17 +42,36 @@ context.fill()
 
 class Asteroid{
   constructor(posX, posY, radius){
-    this.posX=posX;
-    this.posY=posY;
-    this.radius=radius;
+    this.posX=posX
+    this.posY=posY
+    this.radius=radius
   }
   draw(){
-    context.beginPath();
+    context.beginPath()
     context.fillStyle="#C6C7A2"
-    context.arc(this.posX, this.posY, this.radius, 0, 2 * Math.PI);
-    context.fill();
+    context.arc(this.posX, this.posY, this.radius, 0, 2 * Math.PI)
+    context.fill()
   }
 }
 
-let asteroid1 = new Asteroid(50, 50, 20);
-asteroid1.draw()
+// let asteroid1 = new Asteroid(50, 50, 10);
+// asteroid1.draw();
+
+const tRadius=[20, 30, 40]
+const asteroids=new Array(5)
+let posX, posY, radius, indexRadius
+
+function generateAsteroid(){
+  for (let i = 0; i < asteroids.length; i++) {
+    posY=20
+    posX=Math.floor(Math.random()*800)
+    indexRadius=Math.floor(Math.random()*3)
+    radius=tRadius[indexRadius]
+    asteroids[i]=new Asteroid(posX, posY, radius)
+    asteroids[i].draw()
+  }
+}
+
+function asteroidFall(){
+  
+}
