f7be03e4e65551b4d058e4ce30651dd070b8056b
ThirietOscar <47737805+ThirietOscar@users.noreply.github.com>
2019-04-18 17:12:49 +0200

    PageAccueil Vfinale et propre

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,7 @@ let randomX , randomY , randomSize
 let starContainer = document.querySelector('#img__container')
 let stars = new Array()
 
+//create the functions to place the stars at random locations in the window
 function getRandomPosition(element) {
 	var x = document.body.offsetHeight-element.clientHeight
 	var y = document.body.offsetWidth-element.clientWidth
@@ -10,31 +11,32 @@ function getRandomPosition(element) {
 	var randomY = Math.floor(Math.random()*y)
 	return [randomX,randomY]
 }
-for (let i=0; i<200;i++){                   // Creation de 200 images :etoiles
+
+//creates 300 images of stars
+for (let i=0; i<300;i++){
 window.onload=etoileBackground()
   function etoileBackground () {
-    var randomSize = Math.floor(Math.random()*20)
+    var randomSize = Math.floor(Math.random()*22)  //gives a random size to the star
     var star = document.createElement("IMG")
     star.classList.add("star")
     star.setAttribute("src", "images/etoile.png")
-    star.style.filter = "blur(1px)"
-    star.classList.add("rotating")      //ajout d'une classe pour la rotation
+    star.style.filter = "blur(2px)"				//adds blur on all the stars
+    star.classList.add("rotating")      //adds the class rotation to make the stars rotate
     star.style.position = "absolute"
     star.style.transition ="all 1s"
-    starContainer.appendChild(star)
+    starContainer.appendChild(star)		//adds them in a container
     var xy = getRandomPosition(star)
-	  star.style.top = xy[0] + 'px'      //emplacement au hasard
+	  star.style.top = xy[0] + 'px'      //places them randomly
 	  star.style.left = xy[1] + 'px'
-    star.style.width = randomSize + 'px'    //taille au hasard
+    star.style.width = randomSize + 'px'    //gives them a random size
     star.style.height = randomSize + 'px'
   }
-  stars.push(star)
 }
 setInterval(function(){
   stars = document.querySelectorAll('.star')
   console.log(stars)
    for(let i=0; i<stars.length;i++){
-    specialStar = stars[Math.floor(Math.random()*stars.length)]   //selection d'une etoile au hasard
-    specialStar.classList.toggle("specialStar")   //eleve le flou de l'etoile
+    specialStar = stars[Math.floor(Math.random()*stars.length)]   //selects a random star
+    specialStar.classList.toggle("specialStar")   //toggle between blur and not blur
    }
 }, 3000)
