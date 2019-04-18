fcd3893828ff44fb51ead4d2871c7b63351d07cc
AnthonyVilaHetic <anthony.vila@hetic.net>
2019-04-18 14:16:54 +0200

    Revert "Page d'accueil background"
    This reverts commit bf87872f7d05c5fe227e3d265685ffcb9cf4698e.

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,5 @@
-let star , specialStar
+let star
 let randomX , randomY , randomSize
-let starContainer = document.querySelector('#img__container')
-  let stars = new Array()
 
 function getRandomPosition(element) {
 	var x = document.body.offsetHeight-element.clientHeight
@@ -15,30 +13,14 @@ window.onload=etoileBackground()
   function etoileBackground () {
     var randomSize = Math.floor(Math.random()*15)
     var star = document.createElement("IMG")
-    star.classList.add("star")
     star.setAttribute("src", "images/etoile.png")
-    star.style.filter = "blur(1px)"
     star.style.position = "absolute"
-    star.style.transition ="all 1s"
+    let starContainer = document.querySelector('#img__container')
     starContainer.appendChild(star)
-    var xy = getRandomPosition(star)
+    var xy = getRandomPosition(star);
 	  star.style.top = xy[0] + 'px'
 	  star.style.left = xy[1] + 'px'
     star.style.width = randomSize + 'px'
     star.style.height = randomSize + 'px'
   }
-  stars.push(star)
-
 }
-
-
-setInterval(function(){
-  console.log("coucouc")
-  stars = document.querySelectorAll('.star')
-  console.log(stars)
- for(let i=0; i<stars.length;i++){
-  specialStar = stars[Math.floor(Math.random()*stars.length)]
-  specialStar.classList.toggle("specialStar")
-  }
-
-}, 3000)
