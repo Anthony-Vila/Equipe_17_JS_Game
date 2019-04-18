bf87872f7d05c5fe227e3d265685ffcb9cf4698e
ThirietOscar <47737805+ThirietOscar@users.noreply.github.com>
2019-04-18 09:51:54 +0200

    Page d'accueil background
    background etoiles blur

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,7 @@
-let star
+let star , specialStar
 let randomX , randomY , randomSize
+let starContainer = document.querySelector('#img__container')
+  let stars = new Array()
 
 function getRandomPosition(element) {
 	var x = document.body.offsetHeight-element.clientHeight
@@ -13,14 +15,30 @@ window.onload=etoileBackground()
   function etoileBackground () {
     var randomSize = Math.floor(Math.random()*15)
     var star = document.createElement("IMG")
+    star.classList.add("star")
     star.setAttribute("src", "images/etoile.png")
+    star.style.filter = "blur(1px)"
     star.style.position = "absolute"
-    let starContainer = document.querySelector('#img__container')
+    star.style.transition ="all 1s"
     starContainer.appendChild(star)
-    var xy = getRandomPosition(star);
+    var xy = getRandomPosition(star)
 	  star.style.top = xy[0] + 'px'
 	  star.style.left = xy[1] + 'px'
     star.style.width = randomSize + 'px'
     star.style.height = randomSize + 'px'
   }
+  stars.push(star)
+
 }
+
+
+setInterval(function(){
+  console.log("coucouc")
+  stars = document.querySelectorAll('.star')
+  console.log(stars)
+ for(let i=0; i<stars.length;i++){
+  specialStar = stars[Math.floor(Math.random()*stars.length)]
+  specialStar.classList.toggle("specialStar")
+  }
+
+}, 3000)
